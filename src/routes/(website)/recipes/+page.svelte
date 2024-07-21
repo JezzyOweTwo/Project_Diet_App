7e62fc38855df347737ca6db7fc64b123777ba66
git-illite <71469298+git-illite@users.noreply.github.com>
2024-07-20 22:58:39 -0400

    updated fetch command on recipes page to use our mongoDB

--- src/routes/(website)/recipes/+page.svelte
+++ src/routes/(website)/recipes/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import RecipeCard from '../../../components/recipe-card/recipe-card.svelte';
-  import type { Recipe } from '../../../types/types';
+  import { onMount } from "svelte";
+  import RecipeCard from "../../../components/recipe-card/recipe-card.svelte";
+  import type { Recipe } from "../../../types/types";
 
   let recipes: Recipe[] = [];
 
   onMount(async () => {
-    const response = await fetch('https://dummyjson.com/recipes');
+    const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/recipe`);
     const data = await response.json();
     recipes = data.recipes;
   });
@@ -14,7 +14,7 @@
 
 <main>
   <h1>Recipes</h1>
-  {#each recipes as recipe (recipe.id)}
+  {#each recipes as recipe}
     <RecipeCard {recipe} />
   {/each}
 </main>
